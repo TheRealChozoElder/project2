6908482015a09c01a3be9d7a9fd74d0439aa2388
TheRealChozoElder <nathanbeach@mail.weber.edu>
2018-12-13 05:11:08 -0700

    Finished a few todos.

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -2,7 +2,7 @@
     <div class="col-sm-6 col-md-4 col-lg-3">
         <div class="panel panel-default">
             <div class="panel-body quote">
-                <!--ToDo: Add default slot tag for passing the quote-->
+                <slot name="quote"></slot>
             </div>
         </div>
     </div>
@@ -12,11 +12,17 @@
 </script>
 
 <style>
-    /*ToDo: Create a class panel that sets the font-family to preffered font*/
-        /*Set a specific front size to your liking*/
-        /*Set a color of your choice*/
+    panel {
+        font-family: sans-serif;
+        font-size: 16px;
+        color: #00b3ee;
+    }
 
-    /*ToDo: Create a class quote that sets the cursor to a pointer*/
+    quote{
+        cursor: pointer;
+    }
 
-    /*ToDo: Create a hover effect on the class quote changing the background color to a color you prefer*/
+    quote:hover{
+        background-color: yellow;
+    }
 </style>
--- src/components/QuoteGrid.vue
+++ src/components/QuoteGrid.vue
@@ -1,24 +1,31 @@
 <template>
     <div class="row">
         <!--ToDo: Use the Quote component selector-->
-            <!--Loop through the quote and index in quotes-->
-            <!--Add a click event with .native and set it equal to the deleteQuote method created below-->
-            <!--Display the quote inside the appQuote component, in other words pass the data object to the slot in Quote.vue-->
+        <!--Loop through the quote and index in quotes-->
+        <!--Add a click event with .native and set it equal to 
+        the deleteQuote method created below-->
+        <!--Display the quote inside the appQuote component, 
+        in other words pass the data object to the slot in Quote.vue-->
     </div>
 </template>
 
 <script>
-    // ToDo: import the Quote Component
+    import Quote from './components/Quote.vue';
 
     export default {
-        // ToDo: Create props of quote
+        props: {
+            quote
+        },
 
         components: {
-            // ToDo: Initiate the Quote Component with a Custom Selector
+            // ToDo: Initiate the Quote Component with a 
+            // Custom Selector
         },
         methods: {
-            // ToDo: Create a method deleteQuote that takes a paremeter of index
-                // Emit and event listener named quotesDeleted and passes the index parameter
+            // ToDo: Create a method deleteQuote that takes a 
+            // paremeter of index
+            // Emit and event listener named quotesDeleted and 
+            // passes the index parameter
         }
     }
 </script>
