9c81502d626962bd03f3fe4827a55f3b0c3e00b2
TheRealChozoElder <nathanbeach@mail.weber.edu>
2018-12-13 22:02:55 -0700

    Finished header.vue after a bit of research

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,31 +4,22 @@
             <h3>Quotes Added</h3>
             <div class="progress">
                 <div class="progress">
-                    <!--ToDo: Div with class="progress-bar" - 
-                    Bind the style tag that adjusts the style 
-                    width using quoteCount / maxQuotes-->
-                        <!--Remember to multiple the the 
-                        divided total by 100 and adding the '%' 
-                        to get a percentage-->
-                    <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
-                        <!--ToDo: Output the number of quotes 
-                        displayed on the page out of the max quotes-->
-                    </div>
+                    <div class="progress-bar" 
+                         role="progressbar" 
+                         aria-valuenow="60" 
+                         aria-valuemin="0" 
+                         aria-valuemax="100"
+                         :style="{width: (quoteCount / maxQuotes) * 100 + '%'}">
+                         {{ quoteCount }} / {{ maxQuotes }}
                 </div>
             </div>
         </div>
     </div>
+</div>
 </template>
 
 <script>
     export default {
-        // ToDo: Create props that are being passed in 
-        // App.vue (quoteCount & maxQuotes)
-            // Note: use normal array syntax, to pass more 
-            // then 1 separate by comma
-            props:[
-                {quoteCount},
-                {maxQuotes}
-            ]
+        props: ['quoteCount', 'maxQuotes']
     }
 </script>
