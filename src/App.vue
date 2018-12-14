c3ff05ab1e32e69b125c0edb8afcaccfd14753f9
TheRealChozoElder <nathanbeach@mail.weber.edu>
2018-12-13 19:10:29 -0700

    Finished a few more todos again.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
     <div class="container">
         <!--ToDo: Use the Header component selector-->
-        <!--Bind data props quoteCount and set it equal to the quote length-->
-        <!--Bind data props maxQuotes and set it equal to the maxQuotes created below-->
+        <!--Bind data props quoteCount and set it equal to the 
+        quote length-->
+        <!--Bind data props maxQuotes and set it equal to the 
+        maxQuotes created below-->
 
         <!--ToDo: Use the NewQuote component selector-->
-        <!--Initiate an $emit event listener called quoteAdded that is equal to newQuote-->
+        <!--Initiate an $emit event listener called quoteAdded 
+        that is equal to newQuote-->
 
         <!--ToDo: Use the QuoteGrid component selector-->
-        <!--Bind data props quotes and set it equal to the quotes array created below-->
-        <!--Call quoteDeleted that is emitted from the QuoteGrid Component and pass the deleteQuote method created below-->
+        <!--Bind data props quotes and set it equal to the quotes
+        array created below-->
+        <!--Call quoteDeleted that is emitted from the QuoteGrid 
+        Component and pass the deleteQuote method created below-->
         <div class="row">
             <div class="col-sm-12 text-center">
                 <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
@@ -33,16 +38,23 @@
             }
         },
         methods: {
-            // ToDo: Create a method that checks when a newQuote is created and takes the parameter quote.
-            // This method will check if the quotes (Array Data Object) length is greater than or equal to the max quotes allowed
-            // If it is return an alert telling the user to delete a quote
-            // Outside of conditional, push quote (method parameter) to the quotes array
+            // ToDo: Create a method that checks when a newQuote 
+    // is created and takes the parameter quote.
+            // This method will check if the quotes (Array Data 
+            // Object) length is greater than or equal to the max 
+            // quotes allowed
+            // If it is return an alert telling the user to delete 
+            // a quote
+            // Outside of conditional, push quote (method parameter) 
+            // to the quotes array
 
-            // ToDo: Create a method deleteQuote that takes in a parameter of index
+            // ToDo: Create a method deleteQuote that takes in a 
+            // parameter of index
             // This method will splice the quotes array by (index, 1)
         },
         components: {
-            // ToDo: Initiate the QuoteGrid, NewQuote, and Header Components with Custom Selectors
+            // ToDo: Initiate the QuoteGrid, NewQuote, and Header 
+    // Components with Custom Selectors
         }
 
     }
--- src/components/QuoteGrid.vue
+++ src/components/QuoteGrid.vue
@@ -1,11 +1,9 @@
 <template>
     <div class="row">
-        <!--ToDo: Use the Quote component selector-->
-        <!--Loop through the quote and index in quotes-->
-        <!--Add a click event with .native and set it equal 
-        to the deleteQuote method created below-->
-        <!--Display the quote inside the appQuote component, 
-        in other words pass the data object to the slot in Quote.vue-->
+        <quote-selector v-for="(quote, index) in quotes"
+                        @click.native="deleteQuote">
+        </quote-selector>
+        <appQuote>{{ quote }}</appQuote>
     </div>
 </template>
 
@@ -13,21 +11,18 @@
     import Quote from './components/Quote.vue';
 
     export default {
-        // ToDo: Create props of quote
-        props: {
-            quote
-        },
-    
-
+        props: ['quote'],
         components: {
-            // ToDo: Initiate the Quote Component with a Custom 
-            // Selector
+            data: function () {
+                return {
+                    quote: ''
+                }
+            }
         },
         methods: {
-            // ToDo: Create a method deleteQuote that takes a 
-            // paremeter of index
-            // Emit and event listener named quotesDeleted and 
-            // passes the index parameter
+            deleteQuote: function (index) {
+                this.$emit('quotesDeleted', index)
+            }
         }
     }
 </script>
